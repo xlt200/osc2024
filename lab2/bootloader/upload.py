4ba17eef9bb60dded2512ab90362845b0e202fe2
xlt200 <63595977+xlt200@users.noreply.github.com>
2024-03-28 01:11:06 +0800

    Add files via upload

---
+++ lab2/bootloader/upload.py
@@ -0,0 +1,34 @@
+import serial
+import os
+import os.path
+import sys
+
+# s = serial.Serial("/dev/ttyUSB0",baudrate=115200)
+s = serial.Serial("/dev/ttyUSB0",baudrate=115200)
+
+def read_line(s):
+    received_string = ""
+    while True:
+        c = s.read().decode()
+        if c=="\r":
+            continue
+        if c=="\n":
+            break
+        received_string += c
+    return received_string
+
+size = os.stat("shell_kernel.img").st_size
+size_bytes = size.to_bytes(4,"little")
+print(size_bytes)
+# t = 'a'
+# s.write(t.encode())
+s.write(size_bytes)
+
+received_size = read_line(s)
+print(received_size)
+
+with open("shell_kernel.img","rb") as f:
+    s.write(f.read())
+
+received_content = read_line(s)
+print(received_content)
---
+++ lab2/bootloader/utils.c
@@ -0,0 +1,41 @@
+#include "header/utils.h"
+
+int ut_string_compare(const char* str1,const char* str2) {
+  for(;*str1 !='\0'||*str2 !='\0';str1++,str2++){
+    if(*str1 != *str2) return 0;
+    else if(*str1 == '\0' && *str2 =='\0') return 1;
+  }
+  return 1;
+}
+
+unsigned long ut_atoi(const char *s, int char_size) {
+    unsigned long num = 0;
+    for (int i = 0; i < char_size; i++) {
+        num = num * 16;
+        if (*s >= '0' && *s <= '9') {
+            num += (*s - '0');
+        } else if (*s >= 'A' && *s <= 'F') {
+            num += (*s - 'A' + 10);
+        } else if (*s >= 'a' && *s <= 'f') {
+            num += (*s - 'a' + 10);
+        }
+        s++;
+    }
+    return num;
+}
+
+void ut_align(void *size, unsigned int s) {
+	unsigned long* x = (unsigned long*) size;
+	unsigned long mask = s-1;
+	*x = ((*x) + mask) & (~mask);
+}
+
+uint32_t ut_align_up(uint32_t size, int alignment) {
+  return (size + alignment - 1) & ~(alignment-1);
+}
+
+uint32_t ut_strlen(const char *s) {
+    uint32_t i = 0;
+	while (s[i]) i++;
+	return i+1;
+}
